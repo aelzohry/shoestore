0b508bf9f1be4741396db88ff2909d1ae095c57a
Ahmed Elzohry <aelzohry2010@gmail.com>
2022-07-25 16:35:17 +0200

    add host fragment & connect navigation with action bar

--- app/src/main/java/com/udacity/aelzohry/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/aelzohry/shoestore/MainActivity.kt
@@ -3,6 +3,8 @@ package com.udacity.aelzohry.shoestore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.udacity.aelzohry.shoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Connecting navigation with action bar
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
 }
